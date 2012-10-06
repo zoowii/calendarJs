6b0054b4ee212bcd9e3040970e27882254c725de
zoowii <1992zhouwei@gmail.com>
2012-10-06 14:42:13 +0800

    add an example

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 seajs.use(['./js/calendar/calendar.html.js'], function (calendar) {
-	calendar.create($("#calendar")[0], function(dayInfo) { alert(dayInfo.date); });
+	var cal = calendar.create($("#calendar")[0], function(dayInfo) { alert(dayInfo.date); });
+	cal.monthSelect(function(data) {
+		alert(data.month); }
+	);
 });
